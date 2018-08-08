3d8897b7fc6e77fda2aaabe5830c4699507ab589
Bernardo Campos <bernardoguajardo98@hotmail.com>
2018-08-08 08:23:13 -0500

    Falto la base de datos de gitlab2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var routes = require("./routes");
 var passportsetup = require("./passportsetup");
 var app = express();
 
-mongoose.connect("mongodb://mongodb://bernardo:123bernardo@ds113732.mlab.com:13732/gears");
+mongoose.connect("mongodb://bernardo:123bernardo@ds113732.mlab.com:13732/gears");
 //"mongodb://mongodb://bernardo:123bernardo@ds113732.mlab.com:13732/gears"
 //"mongodb://localhost:27017/zombies_nest"
 
